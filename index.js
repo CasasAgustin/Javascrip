ca53d1c19e712ba8df7d27324914ff147b7e71bd
Agustin Casas <agust.1798@gmail.com>
2022-08-30 00:52:24 -0300

    repo

--- index.js
+++ index.js
@@ -3,14 +3,10 @@
 
 let pesos = parseInt(prompt("ingrese el monto a convertir"))
     let dolar = 290;
-    let resultado = "";
+    let resultado = 0;
 
 function conversor(){  
-    if( i = 1 , i < 1000 , i++){
+    if( i = 1 , i < 1000 , i++)
     resultado = pesos * dolar;
-    resultado = resultado
-}
-
 }
  alert("el cambio a dolar es de $" + resultado )
-
