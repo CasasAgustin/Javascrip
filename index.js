775c469be4edd9eec3fe72a3db5039e05c400480
Agustin Casas <agust.1798@gmail.com>
2022-08-30 00:28:17 -0300

    repo

--- index.js
+++ index.js
@@ -1,38 +1,4 @@
-/*
-let importe = (prompt ("ingrese el importe que desea convertir"))
-const dolar = 290;
-//let euro = 300;
-let igual = (prompt(""))
 
-// alert('igrese el importe que quiere convertir')
-
- function convertir(){
-    importe = Number('ingrese el valor')
-
-     let igual = importe * dolar;
-     
-}
-*/
-/*
-let n = parseInt(prompt("ingrese precios"))
-const dolar = 290;
-
-//let desde = parseInt(prompt("ingrese cosas"))
-
-let hasta = parseInt(prompt("blabal hasta donde "))
-
-
-//if (hasta < desde){
-  //  alert ( "ingreso erroneo")
-//}
-    else{
-        for(i = desde ; i = hasta; i ++){
-            let resultado = n * i;
-            let mensaje = `${n} * ${i} = ${resultado}`;
-            alert (mensaje)
-        }
-    }
-*/
 
 
 let pesos = parseInt(prompt("ingrese el monto a convertir"))
