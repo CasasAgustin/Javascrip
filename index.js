db86d7477dad29830ab587f18f83b9d9b660a039
Agustin Casas <agust.1798@gmail.com>
2022-08-29 23:58:07 -0300

    conversor

---
+++ index.js
@@ -0,0 +1,50 @@
+/*
+let importe = (prompt ("ingrese el importe que desea convertir"))
+const dolar = 290;
+//let euro = 300;
+let igual = (prompt(""))
+
+// alert('igrese el importe que quiere convertir')
+
+ function convertir(){
+    importe = Number('ingrese el valor')
+
+     let igual = importe * dolar;
+     
+}
+*/
+/*
+let n = parseInt(prompt("ingrese precios"))
+const dolar = 290;
+
+//let desde = parseInt(prompt("ingrese cosas"))
+
+let hasta = parseInt(prompt("blabal hasta donde "))
+
+
+//if (hasta < desde){
+  //  alert ( "ingreso erroneo")
+//}
+    else{
+        for(i = desde ; i = hasta; i ++){
+            let resultado = n * i;
+            let mensaje = `${n} * ${i} = ${resultado}`;
+            alert (mensaje)
+        }
+    }
+*/
+
+
+let pesos = parseInt(prompt("ingrese el monto a convertir"))
+    let dolar = 290;
+    let resultado = "";
+
+function conversor(){  
+    if( i = 1 , i < 1000 , i++){
+    resultado = pesos * dolar;
+    resultado = resultado
+}
+
+}
+ alert("el cambio a dolar es de $" + resultado )
+
